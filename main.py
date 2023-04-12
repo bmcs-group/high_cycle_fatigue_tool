c9c7adf60ce26445b18b6b7b555b5568ca8650c1
ishomam <ishomam@gmail.com>
2023-04-12 09:25:31 +0200

    Make the plot use TimesNewRoman font and LaTeX style

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
 import os
 from hcft.api import HCFT
+from bmcs_utils.api import set_latex_mpl_format
+
+# To use Sans font in matplotlib
+set_latex_mpl_format()
+
 hcft = HCFT(file_path=os.path.expanduser("~"))
 hcft.configure_traits()
